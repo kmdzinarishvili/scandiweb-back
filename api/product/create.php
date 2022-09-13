bf39d9deaaf20a04a34e4305e7248763aafb26f9
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2022-09-14 03:17:35 +0400

    removing unnecessary ifs, cleaning code

--- api/product/create.php
+++ api/product/create.php
@@ -4,62 +4,26 @@
     header('Access-Control-Allow-Methods: DELETE');
     header('Access-Control-Allow-Headers:Access-Control-Allow-Origin,Content-Type,Access-Control-Allow-Methods, Authorization,X-Requested-With ');
 
-    use App\Objects\Product as Product;
     use App\Config\Database as Database;
+    use App\Objects\Product as Product;
     use App\Objects\DVD as DVD;
     use App\Objects\Furniture as Furniture;
     use App\Objects\Book as Book;
 
-    $database= new Database();
-    $db = $database->connect();
+    // $database= new Database();
+    // $db = $database->connect();
     $product;
     $data = json_decode(file_get_contents("php://input"));
-    if (empty($data)) {
-        echo "Empty data";
+    if (Product::validateInput($data)) {
+        $className ="App\\Objects\\".$data['type'];
+        $product = new $className($data);
+        $created = $product->create();
+        $response["status"]= 200;
+        $response["data"]="";
+        echo json_encode($response);
     } else {
-        if (property_exists($data, "sku")&&
-        property_exists($data, "name")&&
-        property_exists($data, "price")&&
-        property_exists($data, "type")) {
-            $sku = $data->sku;
-            $name = $data->name;
-            $price =  $data->price;
-            $type = $data->type;
-            unset($data->sku);
-            unset($data->name);
-            unset($data->price);
-            unset($data->type);
-            if ($type==="dvd") {
-                $product=new DVD($db);
-            } elseif ($type==="furniture") {
-                $product= new Furniture($db);
-            } elseif ($type==="book") {
-                $product= new Book($db);
-            }
-            if (($type==="dvd"&&
-                 property_exists($data, "size"))||
-                 ($type==="furniture"&&
-                 property_exists($data, "height")&&
-                 property_exists($data, "width")&&
-                 property_exists($data, "length"))||
-                 ($type==="book"&&
-                 property_exists($data, "weight"))) {
-                $created = $product->create(
-                    $sku,
-                    $name,
-                    $price,
-                    $type,
-                    $data
-                );
-                if ($created) {
-                    echo "creation success";
-                } else {
-                    echo "failed creation";
-                }
-            } else {
-                echo "Invalid specific product data";
-            }
-        } else {
-            echo "Invalid product data";
-        }
+        $response["status"]= 400;
+        $resposne["data"]="";
+        $response["errorMessage"]="Invalid input";
+        echo json_encode($response);
     }
--- api/product/read.php
+++ api/product/read.php
@@ -1,22 +1,20 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+    use App\Objects\Product as Product;
+    use App\Config\Database as Database;
 
-use App\Objects\Product as Product;
-use App\Config\Database as Database;
-
-$database= new Database();
-$db = $database->connect();
-$result = Product::readAll($db, "products");
-
-if (!empty($result)) {
-    $response["status"]= 200;
-    $response["data"]=$result;
-    echo json_encode($response);
-} else {
-    $response["status"]= 404;
-    $resposne["data"]="";
-    $response["errorMessage"]="Request successful but no data found";
-    echo json_encode($response);
-}
+    $database= new Database();
+    $db = $database->connect();
+    $results = Product::readAll($db, "products");
+    if (!empty($results)) {
+        $response["status"]= 200;
+        $response["data"]=$results;
+        echo json_encode($response);
+    } else {
+        $response["status"]= 404;
+        $resposne["data"]="";
+        $response["errorMessage"]="Request successful but no data found";
+        echo json_encode($response);
+    }
