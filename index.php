ee482e577c7971d255afe4b2521c1a570f6cf6d8
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2022-09-14 21:28:52 +0400

    testing

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST,  DELETE, OPTIONS");
 header('Content-Type: application/json');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
