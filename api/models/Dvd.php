be5f7971d64875c385c23a546fa8491768eda8af
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2022-09-17 04:02:49 +0400

    re adding Dvd.php (fixing capitalization issue

---
+++ api/models/Dvd.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+class Dvd extends Product
+{
+    private $size;
+
+    public function __construct($db, $attributes)
+    {
+        parent::__construct($db, $attributes);
+        $this->type = 'dvd';
+        $this->size=$attributes['size'];
+    }
+
+    public function create()
+    {
+        $productSuccess = parent::create($this);
+        if (!$productSuccess) {
+            return false;
+        }
+        $sql = 'Insert into dvds
+                    Set sku=:sku,
+                      size=:size';
+        $stmt = $this->conn->prepare($sql);
+        $sku=htmlspecialchars(strip_tags($this->sku));
+        $size=htmlspecialchars(strip_tags($this->size));
+        $stmt->bindParam(':sku', $sku);
+        $stmt->bindParam(':size', $size);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            echo $stmt->error;
+            return false;
+        }
+    }
+}
